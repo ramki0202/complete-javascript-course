1fa718f9990a400e69fa455695980bad48802136
Ramki <ram.ramki0202@gmail.com>
2023-09-24 20:29:00 +0530

    first leason

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,7 @@
+const country = "india";
+const continent = "Asia";
+const population = 150000000;
+console.log(
+  `my country is ${country}, we belonges to ${continent} and we have ${population} people`
+);
+console.log(typeof null);
