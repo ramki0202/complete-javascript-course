56ece4791d8cdd895edd32428f440900831832e8
Ramki <ram.ramki0202@gmail.com>
2023-09-30 07:23:27 +0530

    last update

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,22 @@
-const country = "india";
-const continent = "Asia";
-const population = 150000000;
-console.log(
-  `my country is ${country}, we belonges to ${continent} and we have ${population} people`
-);
-console.log(typeof null);
+// const country = "india";
+// const continent = "Asia";
+// const population = 150000000;
+// console.log(
+//   `my country is ${country}, we belonges to ${continent} and we have ${population} people`
+// );
+// console.log(typeof null);
+
+let day = prompt("Enter Your Day");
+
+if (day === "Monday" || day === "monday") {
+  console.log("Plan the code");
+} else if (
+  day === "Tuesday" ||
+  day === "tuesday" ||
+  day === "Wednesday" ||
+  day === "wednesday"
+) {
+  console.log("execute the code");
+} else {
+  console.log("not a day");
+}
---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,41 @@
+// const jonas = {
+//   firstName: "Jonas",
+//   lastName: "Schmedtmann",
+//   age: 2037 - 1991,
+//   job: "teacher",
+//   friends: ["Michael", "Peter", "Steven"],
+// };
+
+// const string = `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`;
+// console.log(string);
+
+// const jonas = {
+//   firstName: "Jonas",
+//   lastName: "Schmedtmann",
+//   birthYeah: 1991,
+//   job: "teacher",
+//   friends: ["Michael", "Peter", "Steven"],
+//   hasDriversLicense: false,
+
+// calcAge: function (birthYeah) {
+//   return 2037 - birthYeah;
+// },
+//   calcAge: function () {
+//     this.age = 2023 - this.birthYeah;
+//     return this.age;
+//   },
+// };
+// jonas.calcAge();
+// // console.log(jonas.calcAge());
+// console.log(jonas.age);
+// // "Jonas is a 46-year old teacher, and he has a driver's license"
+
+// console.log(
+//   `${jonas.firstName} is a ${jonas.age}-years old teacher, and her has ${
+//     jonas.hasDriversLicense ? "a" : "no"
+//   } driver's license `
+// );
+
+for (let rep = 1; rep >= 1; rep++) {
+  console.log("Lifting weights repetition 1 🏋️‍♀️");
+}
