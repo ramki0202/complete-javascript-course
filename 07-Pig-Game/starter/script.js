05f6d2ed1b2546768f848c80934555bfbb316a15
Ramki <ram.ramki0202@gmail.com>
2024-01-16 06:30:24 +0530

    Practicals for datastructure part 1

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -9,10 +9,12 @@ let player02 = document.querySelector('.player--1');
 const current0 = document.getElementById('current--0');
 const current1 = document.getElementById('current--1');
 const btnHold = document.querySelector('.btn--hold');
+const btnNew = document.querySelector('.btn--new');
 
 let currentScore = 0;
 let activePlayer = 0;
 let score = [0, 0];
+let playing = true;
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -27,35 +29,61 @@ player1ScoreDisplay.innerHTML = 0;
 player2ScoreDisplay.innerHTML = 0;
 
 rollDiceBtn.addEventListener('click', function () {
-  function getRandomNumber() {
-    // Generate a random number between 1 and 6
-    return Math.floor(Math.random() * 6) + 1;
-  }
-  const randomNumber = getRandomNumber();
+  if (playing) {
+    function getRandomNumber() {
+      // Generate a random number between 1 and 6
+      return Math.floor(Math.random() * 6) + 1;
+    }
+    const randomNumber = getRandomNumber();
 
-  dice.classList.remove('hide');
-  // Update the src attribute of the dice image based on the random number
-  dice.src = `dice-${randomNumber}.png`;
-  dice.alt = `Dice ${randomNumber}`;
+    dice.classList.remove('hide');
+    // Update the src attribute of the dice image based on the random number
+    dice.src = `dice-${randomNumber}.png`;
+    dice.alt = `Dice ${randomNumber}`;
 
-  if (randomNumber !== 1) {
-    currentScore += randomNumber;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    switchPlayer();
+    if (randomNumber !== 1) {
+      currentScore += randomNumber;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
   }
 });
 
 console.log('current score Out', currentScore);
 
 btnHold.addEventListener('click', function () {
-  //adding current score to saved score
+  if (playing) {
+    //adding current score to saved score
 
-  score[activePlayer] += currentScore;
+    score[activePlayer] += currentScore;
 
-  document.getElementById(`score--${activePlayer}`).textContent =
-    score[activePlayer];
+    document.getElementById(`score--${activePlayer}`).textContent =
+      score[activePlayer];
 
-  switchPlayer();
+    if (score[activePlayer] >= 100) {
+      playing = false;
+      dice.classList.add('hide');
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', function () {
+  player1ScoreDisplay.innerHTML = 0;
+  player2ScoreDisplay.innerHTML = 0;
+  dice.classList.add('hide');
+  current0.innerHTML = 0;
+  current1.innerHTML = 0;
+  score = [0, 0];
+  playing = true;
 });
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,29 +1,315 @@
-'use strict';
-
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
-// Data needed for first part of the section
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
+const books = [
+  {
+    title: 'Algorithms',
+    author: ['Robert Sedgewick', 'Kevin Wayne'],
+    publisher: 'Addison-Wesley Professional',
+    publicationDate: '2011-03-24',
+    edition: 4,
+    keywords: [
+      'computer science',
+      'programming',
+      'algorithms',
+      'data structures',
+      'java',
+      'math',
+      'software',
+      'engineering',
+    ],
+    pages: 976,
+    format: 'hardcover',
+    ISBN: '9780321573513',
+    language: 'English',
+    programmingLanguage: 'Java',
+    onlineContent: true,
+    thirdParty: {
+      goodreads: {
+        rating: 4.41,
+        ratingsCount: 1733,
+        reviewsCount: 63,
+        fiveStarRatingCount: 976,
+        oneStarRatingCount: 13,
+      },
     },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
+    highlighted: true,
+    printBookInfo: function ({ title, author, year = 'year unknown' }) {
+      console.log(`${title} by ${author} , ${year}`);
     },
   },
+  {
+    title: 'Structure and Interpretation of Computer Programs',
+    author: [
+      'Harold Abelson',
+      'Gerald Jay Sussman',
+      'Julie Sussman (Contributor)',
+    ],
+    publisher: 'The MIT Press',
+    publicationDate: '2022-04-12',
+    edition: 2,
+    keywords: [
+      'computer science',
+      'programming',
+      'javascript',
+      'software',
+      'engineering',
+    ],
+    pages: 640,
+    format: 'paperback',
+    ISBN: '9780262543231',
+    language: 'English',
+    programmingLanguage: 'JavaScript',
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 4.36,
+        ratingsCount: 14,
+        reviewsCount: 3,
+        fiveStarRatingCount: 8,
+        oneStarRatingCount: 0,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: "Computer Systems: A Programmer's Perspective",
+    author: ['Randal E. Bryant', "David Richard O'Hallaron"],
+    publisher: 'Prentice Hall',
+    publicationDate: '2002-01-01',
+    edition: 1,
+    keywords: [
+      'computer science',
+      'computer systems',
+      'programming',
+      'software',
+      'C',
+      'engineering',
+    ],
+    pages: 978,
+    format: 'hardcover',
+    ISBN: '9780130340740',
+    language: 'English',
+    programmingLanguage: 'C',
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 4.44,
+        ratingsCount: 1010,
+        reviewsCount: 57,
+        fiveStarRatingCount: 638,
+        oneStarRatingCount: 16,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: 'Operating System Concepts',
+    author: ['Abraham Silberschatz', 'Peter B. Galvin', 'Greg Gagne'],
+    publisher: 'John Wiley & Sons',
+    publicationDate: '2004-12-14',
+    edition: 10,
+    keywords: [
+      'computer science',
+      'operating systems',
+      'programming',
+      'software',
+      'C',
+      'Java',
+      'engineering',
+    ],
+    pages: 921,
+    format: 'hardcover',
+    ISBN: '9780471694663',
+    language: 'English',
+    programmingLanguage: 'C, Java',
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 3.9,
+        ratingsCount: 2131,
+        reviewsCount: 114,
+        fiveStarRatingCount: 728,
+        oneStarRatingCount: 65,
+      },
+    },
+  },
+  {
+    title: 'Engineering Mathematics',
+    author: ['K.A. Stroud', 'Dexter J. Booth'],
+    publisher: 'Palgrave',
+    publicationDate: '2007-01-01',
+    edition: 14,
+    keywords: ['mathematics', 'engineering'],
+    pages: 1288,
+    format: 'paperback',
+    ISBN: '9781403942463',
+    language: 'English',
+    programmingLanguage: null,
+    onlineContent: true,
+    thirdParty: {
+      goodreads: {
+        rating: 4.35,
+        ratingsCount: 370,
+        reviewsCount: 18,
+        fiveStarRatingCount: 211,
+        oneStarRatingCount: 6,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: 'The Personal MBA: Master the Art of Business',
+    author: 'Josh Kaufman',
+    publisher: 'Portfolio',
+    publicationDate: '2010-12-30',
+    keywords: ['business'],
+    pages: 416,
+    format: 'hardcover',
+    ISBN: '9781591843528',
+    language: 'English',
+    thirdParty: {
+      goodreads: {
+        rating: 4.11,
+        ratingsCount: 40119,
+        reviewsCount: 1351,
+        fiveStarRatingCount: 18033,
+        oneStarRatingCount: 1090,
+      },
+    },
+  },
+  {
+    title: 'Crafting Interpreters',
+    author: 'Robert Nystrom',
+    publisher: 'Genever Benning',
+    publicationDate: '2021-07-28',
+    keywords: [
+      'computer science',
+      'compilers',
+      'engineering',
+      'interpreters',
+      'software',
+      'engineering',
+    ],
+    pages: 865,
+    format: 'paperback',
+    ISBN: '9780990582939',
+    language: 'English',
+    thirdParty: {
+      goodreads: {
+        rating: 4.7,
+        ratingsCount: 253,
+        reviewsCount: 23,
+        fiveStarRatingCount: 193,
+        oneStarRatingCount: 0,
+      },
+    },
+  },
+  {
+    title: 'Deep Work: Rules for Focused Success in a Distracted World',
+    author: 'Cal Newport',
+    publisher: 'Grand Central Publishing',
+    publicationDate: '2016-01-05',
+    edition: 1,
+    keywords: ['work', 'focus', 'personal development', 'business'],
+    pages: 296,
+    format: 'hardcover',
+    ISBN: '9781455586691',
+    language: 'English',
+    thirdParty: {
+      goodreads: {
+        rating: 4.19,
+        ratingsCount: 144584,
+        reviewsCount: 11598,
+        fiveStarRatingCount: 63405,
+        oneStarRatingCount: 1808,
+      },
+    },
+    highlighted: true,
+  },
+];
+
+const [, , thirdBook] = books;
+
+// console.log(thirdBook);
+
+const ratings = [
+  ['rating', 4.19],
+  ['ratingsCount', 144584],
+];
+
+// const [rating, ratingsCount] = ratings;
+const [[, rating], [, ratingsCount]] = ratings;
+// console.log(rating, ratingsCount);
+
+const ratingStars = [63405, 1808];
+
+const [fiveStarRatings, oneStarRatings, threeStarRatings = 0] = ratingStars;
+// console.log(fiveStarRatings, oneStarRatings, threeStarRatings);
+
+const { title, author, ISBN } = books[0];
+
+// console.log(title, author, ISBN);
+
+const { keywords: tags } = books[0];
+
+// console.log(tags);
+// console.log(books[0].keywords);
+
+const { language, programmingLanguage: unknown } = books[6];
+
+// console.log(books[6].language);
+let bookTitle = 'unknown';
+let bookAuthor = 'unknown';
+
+// books[0].title = bookTitle;
+// books[0].author =bookAuthor;
+
+// ({ title: bookTitle, author: bookAuthor } = books[0]);
+// ({title: bookTitle, author: bookAuthor} = books[0]);
+
+// console.log(bookAuthor, bookTitle);
+
+// books[0].printBookInfo({ title, author, year: unknown });
+
+const bookAuthors = [...books[0].author, ...books[1].author];
+// const bookAuthors = [...books[0].author, ...books[1].author];
+// console.log(bookAuthors);
+
+const spellWord = function (word) {
+  console.log(...word);
 };
+
+// spellWord('ramki');
+// const [mainKeyword, ...rest] = books[0].keywords;
+// console.log(mainKeyword, rest);
+const { publisher: bookPublisher, ...restOfTheBook } = books[1];
+// console.log(bookPublisher, restOfTheBook);
+
+// [ title: title, author: authors]  = books[0];
+
+const printBookAuthorsCount = function ({ title, ...authors }) {
+  console.log(`The book "${title}" has ${authors.length} authors`);
+};
+
+// console.log({ title, author });
+
+const hasExamplesInJava = function (arg) {
+  return arg.programmingLanguage === 'Java' || 'no data available';
+};
+
+// console.log(hasExamplesInJava(books[7]));
+
+// for (let i = 0; i <= books.length; i++) {
+//   books[i].onlineContent === true ? `${books[i].title}" provides online content` || " ";
+// }
+for (let i = 0; i < books.length; i++) {
+  books[i].onlineContent &&
+    console.log(`${books[i].title} has online content.`);
+}
+
+for (let i = 0; i < books.length; i++) {
+  const hasOnlineContent = books[i].onlineContent ?? false;
+  console.log(
+    `${books[i].title} ${
+      hasOnlineContent ? 'has' : 'provides no'
+    } data about its online content.`
+  );
+}
